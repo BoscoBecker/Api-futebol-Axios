d2884bc6ce36fc1afacde474fc5c95089a810713
João bosco <joao.jbosco@gmail.com>
2022-05-09 09:33:51 -0300

    fix dotenv setting
    fix dotenv setting

--- src/config/key.js
+++ src/config/key.js
@@ -1,10 +1,2 @@
-var  httpAxios = require('axios');
 const dotenv = require('dotenv');
       dotenv.config();
-
- httpAxios = new httpAxios.create({
-      baseURL: `${process.env.API_URL}`,
-      headers: { 'Authorization': `Bearer ${process.env.API_KEY}`}
-      });
-
-module.export = httpAxios;    
